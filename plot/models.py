126081c9f0420c79ec28e31a8ca50d470562f494
grv07 <grvtyagi22@gmail.com>
2016-11-13 21:04:02 +0530

    change my database because of insert lock error on sqllite DB

--- plot/models.py
+++ plot/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Phone(models.Model):
     """Phone Class to handle phone data from api call"""    
-    pos_x = models.FloatField(default=0, editable=False) 
-    pos_y = models.FloatField(default=0, editable=False)
-    rssi = models.FloatField(default=0, editable=False)
-    time = models.FloatField(default=0, editable=False)
-    imsi =models.CharField(max_length=30, editable=False)
+    pos_x = models.FloatField(default=0) 
+    pos_y = models.FloatField(default=0)
+    rssi = models.FloatField(default=0)
+    time = models.FloatField(default=0)
+    imsi = models.CharField(max_length=30, blank=False)
 
     def save(self, *args, **kwargs):
         # Code to custom save
         super(Phone, self).save(*args, **kwargs)
 
-    # Validate before save. 
-    def validate_data(self, **kwargs):
-        pass
+
+class PhoneForm(ModelForm):
+    class Meta:
+        model = Phone
+        fields = '__all__'    
+
 # Create your models here.
--- plot/views.py
+++ plot/views.py
@@ -1,29 +1,36 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-# from django.core import serializers
 
-from plot.models import Phone
+from plot.models import Phone, PhoneForm
 
 import json 
 
+# Get data of phone ...
 def get_live_data(request):
-	my_queryset = Phone.objects.all().order_by('id')[int(request.GET.get('offset')):int(request.GET.get('limit'))]
-	result_list = list(my_queryset.values('pos_x', 'pos_y', 'imsi', 'rssi', 'time'))
-	# Update json accor..
-	# result_list = [{'x':data.pos_x, 'y':data.pos_y, 'name':data.imsi, 'z':2} for data in  my_queryset]
-	return JsonResponse(json.dumps(result_list), safe=False)
+	try:
+		# In a case offset and limit not given default offset=0, limit=100
+		my_queryset = Phone.objects.all().order_by('id')[
+			int(request.GET.get('offset', 0)):int(request.GET.get('limit', 100))]
+		result_list = list(my_queryset.values('pos_x', 'pos_y', 'imsi', 'rssi', 'time'))
+		# If Update json accor..
+		# result_list = [{'x':data.pos_x, 'y':data.pos_y, 'name':data.imsi, 'z':2} for data in  my_queryset]
+		return JsonResponse(json.dumps(result_list), safe=False)
+	except Exception as e:
+		return JsonResponse({'msg':'Out of data'}, safe=False, status=500)	
 
 @csrf_exempt
 def plot_data(request):
+	# Save data on POST call ..
+	print(request.POST)
 	if request.method == 'POST':
-		values = request.POST.dict()
-		try:
-			Phone.objects.create(**values)
-			return JsonResponse({'status-msg': 'Success'})
-		except Exception as e:
-			print(e.args)
-			return JsonResponse({'status-msg': 'Fail', 'status':500})
+		pf = PhoneForm(request.POST)
+		# Validate all POST data user send
+		if pf.is_valid():
+			obj = pf.save()
+			return JsonResponse({'msg': 'Success', 'id': obj.id})
+		else:
+			return JsonResponse({'msg': 'Fail to save.','errors': dict(pf.errors.items())}, status=500)	
 	else:
 		return render(request, 'plot_data.html')
 
